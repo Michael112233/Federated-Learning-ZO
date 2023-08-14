17189483eaaef511dd4641b0883c760a49a77fad
Michael233 <920235063@qq.com>
2023-08-14 16:07:06 +0800

    new

--- src/federated_main.py
+++ src/federated_main.py
@@ -5,21 +5,10 @@
 
 import random
 import time
-import pickle
 import numpy as np
-from matplotlib import pyplot as plt
 from sampling import mnist_dataset
 from models import LRmodel
-from tqdm import tqdm
 import scipy.io as sio
-
-import torch
-from tensorboardX import SummaryWriter
-
-from options import args_parser
-from update import LocalUpdate, test_inference
-from models import MLP, CNNMnist, CNNFashion_Mnist, CNNCifar, LRmodel
-from utils import get_dataset, average_weights, exp_details
 
 def update_client(weights):
     for i in range(local_iteration):
@@ -30,43 +19,6 @@ def update_client(weights):
 
 if __name__ == '__main__':
     start_time = time.time()
-
-    # define paths
-    # path_project = os.path.abspath('..')
-    # logger = SummaryWriter('../logs')
-    #
-    # args = args_parser()
-    # exp_details(args)
-    #
-    # if args.gpu:
-    #     torch.cuda.set_device(args.gpu_id)
-    # device = 'cuda' if args.gpu else 'cpu'
-    #
-    # # load dataset and user groups
-    # train_dataset, test_dataset, user_groups = get_dataset(args)
-    #
-    # # BUILD MODEL
-    # if args.model == 'cnn':
-    #     # Convolutional neural network
-    #     if args.dataset == 'mnist':
-    #         global_model = CNNMnist(args=args)
-    #     elif args.dataset == 'fmnist':
-    #         global_model = CNNFashion_Mnist(args=args)
-    #     elif args.dataset == 'cifar':
-    #         global_model = CNNCifar(args=args)
-    #
-    # elif args.model == 'mlp':
-    #     # Multi-layer preceptron
-    #     img_size = train_dataset[0][0].shape
-    #     len_in = 1
-    #     for x in img_size:
-    #         len_in *= x
-    #         global_model = MLP(dim_in=len_in, dim_hidden=64,
-    #                            dim_out=args.num_classes)
-    # elif args.model == 'logistic': # add for logistic regression
-    #     global_model = LRmodel(784, 10) # add for logistic regression
-    # else:
-    #     exit('Error: unrecognized model')
     mnist_data = sio.loadmat('data/mnist/mnist.mat')
     mnist = mnist_dataset(database=mnist_data)
     global_model = LRmodel()
